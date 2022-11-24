09fd30d1ad820f22cf785ec1333f52ff22a957f3
sulfranc <sulfranc@gmail.com>
2022-11-24 12:49:24 -0500

    commit tree 790054bb630b08055e1cca1eeedf78ff6f2f2054

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,22 @@
-class Student 
+require_relative './user'
+
+class Student < User
+    def initialize 
+    @knowledge =[]
+
+     def learn (word)
+      @knowledge << word
+     end
+
+     def knowledge
+       @knowledge
+
+     end
+    
+
+    end
+    
+
+   
 
 end
--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,6 +1,13 @@
-class Teacher
+require_relative './user'
+class Teacher < User
 
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
 
+  def teach 
+   array_length =  KNOWLEDGE.length
+   random_num = rand(1..array_length)
+   KNOWLEDGE[random_num]
+
+  end
   
 end
